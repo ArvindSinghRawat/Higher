67c421fdbe59543ea78980b23f07a01805c96964
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2023-01-27 16:48:34 +0530

    Changed styling for calendar item

--- src/component/calendar/item/CalendarItemComponent.tsx
+++ src/component/calendar/item/CalendarItemComponent.tsx
@@ -9,7 +9,7 @@ type CalendarItemComponentProps = {
 
 /**
  * Card which can be used as a calendar entry
- * @param label - Label that is actually renedered on the component 
+ * @param label - Label that is actually renedered on the component
  * @param is_muted - If true, mutes the rendered component
  * @returns A single component which has a label and conditionally muted
  */
@@ -17,11 +17,23 @@ export default function CalendarItemComponent({
   label,
   is_muted = false,
 }: CalendarItemComponentProps) {
-  let classes = "block m-3 text-center";
+  let classes = "block m-2 lg:m-4 2xl:m-5 p-2 lg:p-4 2xl:p-5 p-2 h-full w-full";
+  const muted_gradient = {
+    background: "rgb(243,244,246)",
+    backgroundImage: `repeating-linear-gradient(to left top, #f3f4f6, #ffffff, #f3f4f6 50px)`,
+    filter: `progid:DXImageTransform.Microsoft.gradient(startColorstr="#f3f4f6",endColorstr="#f3f4f6",GradientType=1)`,
+  };
   if (is_muted) {
-    classes += " bg-gradient-to-tl from-white via-gray-100 to-white bg-repeat-x	";
-    return <Card className={classes}><Text type="secondary">{label}</Text></Card>
+    return (
+      <Card size="small" hoverable={true} className={classes} style={muted_gradient}>
+        <Text>{label}</Text>
+      </Card>
+    );
   } else {
-    return <Card className={classes}><Text>{label}</Text></Card>;
+    return (
+      <Card size="small" className={classes} hoverable={true}>
+        <Text className="block object-left-top font-medium">{label}</Text>
+      </Card>
+    );
   }
 }
